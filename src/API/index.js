42192d2630053b8e66c75e84aa148ac3e4093ac4
niteshkumar78 <niteshkumartg@gmail.com>
2022-01-29 18:38:31 +0530

    updated

--- src/API/index.js
+++ src/API/index.js
@@ -1,1 +1,2 @@
-export const baseurl = " https://eat-healthy-backend.herokuapp.com";
+// export const baseurl = " https://eat-healthy-backend.herokuapp.com";
+export const baseurl = " http://localhost:3000";
